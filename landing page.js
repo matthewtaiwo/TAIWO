d9ffc7fa58b6bde28992a5bc03fc303a189eab71
matthewtaiwo <matthewogunyebi26@gmail.com>
2023-09-19 18:12:30 +0100

    adding all file

---
+++ landing page.js
@@ -0,0 +1,26 @@
+// const imageContainerEL = document.queryselector(".image-container");
+
+// const prevEL = document.getElementById("prev");
+// const nextEL = document.getElementById("next");
+// let x = 0;
+// let timer;
+// prevEL.addEventListener("click", () => {
+//     x = x + 45;
+//     clearTimeout(timer);
+//     updategallery()
+// });
+// nextEL.addEventListener("click", () => {
+//     x = x + 45;
+//     clearTimeout(timer);
+//     updategallery()
+// });
+
+// function updategallery(){
+//     imageContainerEL.style.transform = 'prespective(1000px) rotateY(${x}deg)';
+//     timer = setTimeout(() => {
+//         x = x - 45;
+//         updategallery();
+//     },3000);
+// }
+
+// updategallery();
